e4b068721423b8685e5dceb95b5be48764c60206
Brian <bkimmle@gmail.com>
2026-01-24 12:08:30 -0600

    Removed pugixml dependency

--- Dependencies.cmake
+++ Dependencies.cmake
@@ -2,10 +2,8 @@
 set(WARP_VERSION     v2.0.1)
 set(GLAZE_VERSION    v7.0.0)
 set(HTTPLIB_VERSION  v0.30.1)
-set(PUGIXML_VERSION  v1.15)
 
 # Repository URLs (optional, but good for one-spot editing)
 set(WARP_REPO     https://github.com/brikim/warp.git)
 set(GLAZE_REPO    https://github.com/stephenberry/glaze.git)
 set(HTTPLIB_REPO  https://github.com/yhirose/cpp-httplib.git)
-set(PUGIXML_REPO  https://github.com/zeux/pugixml.git)
