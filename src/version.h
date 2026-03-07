c075ff16c812f229e7e97cd4f4ec47721380c9b7
Brian <bkimmle@gmail.com>
2026-03-07 09:43:12 -0600

    warp udpate to handle new cron scheduler

--- src/version.h
+++ src/version.h
@@ -4,5 +4,5 @@
 namespace remote_scan
 {
    inline constexpr std::string_view REMOTE_SCAN_NAME("Remote-Scan");
-   inline constexpr std::string_view REMOTE_SCAN_VERSION("1.5.3");
+   inline constexpr std::string_view REMOTE_SCAN_VERSION("1.6.0");
 }
